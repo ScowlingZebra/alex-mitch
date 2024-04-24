930195d52b30c99f247d68129da8fb3fc87a04da
ScowlingZebra <66571547+ScowlingZebra@users.noreply.github.com>
2024-04-24 01:19:40 -0500

    Update comment-widget.js

--- js/comment-widget-dark/comment-widget.js
+++ js/comment-widget-dark/comment-widget.js
@@ -17,7 +17,7 @@
 */
 
 // The values in this section are REQUIRED for the widget to work! Keep them in quotes!
-const s_stylePath = href='./js/comment-widget-dark/comment-widget-dark.css';
+const s_stylePath = <link rel="stylesheet" type="text/css" href="./style.css">;
 const s_formId = '1FAIpQLSc_zCQgOofDXSHQBd2dEGK9Xcs3vjDrBnPeFFoIzMbP-kwuqA';
 const s_nameId = '613787050';
 const s_websiteId = '2142749937';
